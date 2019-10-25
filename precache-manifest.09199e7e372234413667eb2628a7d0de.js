3dc709a4ba441fce9135b54df6f3fc28b2a0be80
DESKTOP-GHPUBQT\gogos <gogosergey@gmail.com>
2019-10-25 22:33:16 +0300

    Updates

---
+++ precache-manifest.09199e7e372234413667eb2628a7d0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac822801f9e71fed0cc06c6bdc53322d",
+    "url": "/voxertech/index.html"
+  },
+  {
+    "revision": "d8b41e97d533048ba288",
+    "url": "/voxertech/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6d5cbdae2b843f0d3d78",
+    "url": "/voxertech/static/css/main.4afafc87.chunk.css"
+  },
+  {
+    "revision": "d8b41e97d533048ba288",
+    "url": "/voxertech/static/js/2.a838a450.chunk.js"
+  },
+  {
+    "revision": "6d5cbdae2b843f0d3d78",
+    "url": "/voxertech/static/js/main.a35c4cec.chunk.js"
+  },
+  {
+    "revision": "4d08122b26e108cc20da",
+    "url": "/voxertech/static/js/runtime-main.a244b83c.js"
+  }
+]);
